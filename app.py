4c7eeea0920a8879c85f994586c0ab01faa8fdff
Cornelius Karel Halim <62299784+corneliuskarel@users.noreply.github.com>
2025-12-11 21:18:29 +0700

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import pandas as pd
 
 st.title("Auto-Append Demo (GitHub Actions → Streamlit)")
 
-RAW_URL = "https://github.com/corneliuskarel/BitcoinForecasting_/blob/main/data.csv"
+RAW_URL = "https://raw.githubusercontent.com/corneliuskarel/BitcoinForecasting_/main/data.csv"
 
 st.write("Reading CSV from GitHub:")
 st.code(RAW_URL)
